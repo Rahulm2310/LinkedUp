29ba6bbae5efe41c6d9182d125092fd4f0cc6d76
Your Name <you@example.com>
2020-01-12 14:53:27 +0530

    initial commit with react

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
+//init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('Api running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.port || 5000;
 
